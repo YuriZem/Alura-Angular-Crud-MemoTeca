4171fe54c574cc6b78a6c0c7213e983b0374cf7b
yurizem <yuri.vinicius.dev333@gmail.com>
2023-06-17 05:55:39 -0300

    iniciando 2 curso do mesmo projeto agora estudando sobre formularios reativos

--- memoteca/src/app/componentes/pensamentos/criar-pesansamento/criar-pesansamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pesansamento/criar-pesansamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pesansamento',
@@ -15,14 +16,21 @@ export class CriarPesansamentoComponent implements OnInit {
     autoria: '',
     modelo: 'modelo2'
   }
+
+  formulario!: FormGroup;
+
   constructor(
      private service: PensamentoService,
      private router: Router,
-
-
-     ) { }
+     private formBuilder:FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulario Reativo'],
+      autoria: [''],
+      modelo: ['modelo1'],
+    })
   }
 
   criarPensamento(){
