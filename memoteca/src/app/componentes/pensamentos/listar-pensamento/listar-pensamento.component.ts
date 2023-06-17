ced2fcf23d7c2c2378f76c0862ab616868fa550e
yurizem <yuri.vinicius.dev333@gmail.com>
2023-06-17 04:06:03 -0300

    Criando Componente de pensamento e adicionando diretivas no html

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+    conteudo: 'I love Angularxxxx',
+    autoria: 'Yuriss',
+    modelo: 'modelo1'
+    },
+    {
+    conteudo: 'I love Angularxxxx',
+    autoria: 'Yuriss',
+    modelo: 'modelo2'
+    },
+    {
+    conteudo: 'kakakakakkkakakakskdjsakdj sakdlsdjkajdkajsdklakakakakakkkakakakskdjsakdjsa kdlsdjkajdkajsdklakakakakakkkakakakskdj sakdjsakdlsdjkajdkajsdklakakakakakkkakakaksk djsakdjsakdlsdjkajdkajsdklakakakak akkkakakakskdjsakdjsakdlsdjkajd ajsdklafdusifuidufisofuoisdfudis fuiufdikakakakakkkakakaks kdjsakdjsakdlsdjkajdkajsdkla',
+    autoria: 'Ys',
+    modelo: 'modelo3'
+    },
+]
   constructor() { }
 
   ngOnInit(): void {
